1ef2cf16d426a4edf3d38245f04af1de3467e637
akhfa <akhmadfakhoni@gmail.com>
2015-05-11 05:07:45 +0700

    added id to nik textbox

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -4,7 +4,7 @@
   		var d=document.getElementById("loginLink");
   		d.className = d.className + " active";
 	</script>
-	
+
 	<link href="css/loginStyle.css" rel='stylesheet' type='text/css' />
 	<link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
 	<div class="container">
@@ -12,37 +12,23 @@
 			<form id="loginForm" action="{{url('login')}}" method="POST">
 				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 				<li>
-                    <input class="formlogin" name="nik" type="text" class="text" placeholder="NIK" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'nik';}"> </input>
+                    <input id = "nik" class="formlogin" name="nik" type="text" class="text" placeholder="NIK" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'nik';}"> </input>
 				</li>
 				<li>
 					<input id="password" name="password" type="password" placeholder="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"> </input>
 				</li>
 				<div class="p-container">
-						<input type="submit" onclick="#" value="SIGN IN" >
+						<input type="submit" value="SIGN IN" >
 						<div class="clear"> </div>
 				</div>
 			</form>
 		</div>
 	</div>
 
-	<!--<script>
-		function validateForm() {
-			var usr = document.getElementById("username.value");
-			var pwd = document.getElementById("password.value");
-
-			if (usr==="" || pwd=="") {
-				alert('username and password must be filled!');
-				return false;
-			}
-
-			return true;
-		}
-	</script>-->
-
 @stop
 
 @section('javascript')
-    $('#loginForm').submit(function(e) {
+$('#loginForm').submit(function(e) {
     var nik = $('#nik').val();
     var password = $('#password').val()
     $.ajax({
